ccd5f0c9f5094288f8bab2a4739457e6efd25f1e
YuliaSilk <yuliashevchyk0506@gmail.com>
2025-10-08 12:16:58 +0300

    fixed link to page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
+import {NavLink} from "react-router-dom";
+
 export const Home = () => {
  return (
   <section className="container w-full flex flex-col items-center h-screen py-72">
    <h1 className="text-4xl font-bold">Home</h1>
    <p>
     It's a home page. You need to go to{" "}
-    <a
-     className="text-primary"
-     href="/profile"
+    <NavLink
+     to="/profile"
+     className="py-3 border-b border-gray-100 text-primary"
     >
-     Stripe profile
-    </a>
+     Профіль перевізника
+    </NavLink>
    </p>
   </section>
  );
